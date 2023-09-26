d39807cd7e5a4e01b44686675bfbdfbbc4c3472c
Fadi <fadybd1@gmail.com>
2023-09-26 21:08:51 +0300

    ui: refactored componenets places in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container, Grid } from "@mui/material";
-import Projects from "components/Work/Work";
+import Work from "components/Work/Work";
 import Skills from "components/Skills/Skills";
 import Contact from "components/Contact/Contact";
 import Footer from "components/Footer/Footer";
 import Hero from "components/Hero/Hero";
 import About from "components/About/About";
+import { MaskedTypography } from "components/core/MaskedTypography";
+import Experience from "components/Experience/Experience";
 
 const Home = () => {
   return (
@@ -26,13 +28,19 @@ const Home = () => {
       {/* Main Content */}
       <Grid container spacing={3}>
         <Grid item xs={12}>
+          <MaskedTypography variant="h4" gutterBottom align="center">
+            About Me
+          </MaskedTypography>
           <About />
         </Grid>
-        <Grid item xs={12}>
-          <Projects />
+        <Grid item xs={12} md={7}>
+          <Skills />
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <Experience />
         </Grid>
         <Grid item xs={12}>
-          <Skills />
+          <Work />
         </Grid>
         <Grid item xs={12}>
           <Contact />
