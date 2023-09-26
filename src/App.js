007ab5bbab3c1dded44656644699295945574d7e
Fadi <fadybd1@gmail.com>
2023-09-26 12:23:35 +0300

    added dependencies and utils

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import CssBaseline from "@mui/material/CssBaseline";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ src/utilities/Colors.js
@@ -0,0 +1,11 @@
+const Colors = {
+  deepIndigo: "#4B0082",
+  vibrantPurple: "#9400D3",
+  darkPurple: "#1B0126",
+  grey: "#808080",
+  softLavender: "#EDE7F6",
+  white: "#FFFFFF",
+  lightGrey: "#D3D3D3",
+};
+
+export default Colors;
