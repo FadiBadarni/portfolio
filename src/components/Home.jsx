7ae980767754da11d64e0db489706f52c1d24a0b
Fadi <fadybd1@gmail.com>
2023-09-26 15:06:59 +0300

    style: added a masked typography component.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Container, Typography, Grid } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import { motion } from "framer-motion";
 import Projects from "./Projects";
 import Skills from "./Skills";
 import Contact from "./Contact";
 import Footer from "./Footer";
+import { MaskedTypography } from "./MaskedTypography";
 
 const Home = () => {
   const textMotionVariant = {
@@ -18,7 +19,10 @@ const Home = () => {
       <Grid
         container
         spacing={4}
-        sx={{ minHeight: "100vh", alignItems: "center" }}
+        sx={{
+          minHeight: "100vh",
+          alignItems: "center",
+        }}
       >
         <Grid item xs={12} sm={6}>
           <motion.div
@@ -26,12 +30,12 @@ const Home = () => {
             animate="visible"
             variants={textMotionVariant}
           >
-            <Typography variant="h1" gutterBottom>
-              Hello, I'm [Your Name]
-            </Typography>
-            <Typography variant="h5">
-              A Software Engineer specialized in [Your Skills]
-            </Typography>
+            <MaskedTypography variant="h1" gutterBottom>
+              Hello, I'm Fadi
+            </MaskedTypography>
+            <MaskedTypography variant="h5">
+              A Software Engineer specialized in Web Development
+            </MaskedTypography>
           </motion.div>
         </Grid>
       </Grid>
---
+++ src/components/MaskedTypography.js
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+import { Typography } from "@mui/material";
+import Colors from "../utilities/Colors";
+
+export const MaskedTypography = styled(Typography)`
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-image: linear-gradient(
+      45deg,
+      ${Colors.aestheticSilver},
+      ${Colors.aestheticSilver}
+    ),
+    linear-gradient(135deg, ${Colors.cultered}, ${Colors.darkPurple});
+  background-blend-mode: screen, overlay;
+`;
