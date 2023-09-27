947bd48595a747e2c4674df2ca23a78202af97e1
Fadi <fadybd1@gmail.com>
2023-09-27 10:54:12 +0300

    feat: skill experience section

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,16 +1,95 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import {
+  Typography,
+  Box,
+  Grid,
+  Stack,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
 import { EmorphedBox } from "components/core/EmorphedBox";
+import { experienceData } from "./experienceData";
+import { StyledChip } from "components/core/StyledChip";
 
-const Experience = () => {
+const Experience = ({ selectedSkill }) => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMediumScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  const skillExperience = experienceData[selectedSkill] || {};
+
   return (
-    <EmorphedBox>
-      <Typography variant="h6" gutterBottom>
-        Experience
-      </Typography>
-      <Typography variant="body1">
-        A collection of my latest Experience.
-      </Typography>
+    <EmorphedBox
+      sx={{
+        height: "100%",
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography
+            variant={isSmallScreen ? "body2" : "h6"}
+            gutterBottom
+            sx={{ fontWeight: "bold", textAlign: "center" }}
+          >
+            {selectedSkill
+              ? `Knowledge about ${selectedSkill}`
+              : "Select a Skill to Display Knowledge"}
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Box
+            sx={{
+              p: 2,
+              borderRadius: "8px",
+              boxShadow: "0px 2px 4px rgba(255, 255, 255, 0.1)",
+            }}
+          >
+            {Object.keys(skillExperience).includes("Overview") && (
+              <Typography
+                variant={isSmallScreen ? "body2" : "body1"}
+                gutterBottom
+              >
+                {skillExperience["Overview"]}
+              </Typography>
+            )}
+          </Box>
+        </Grid>
+
+        {Object.keys(skillExperience).includes("Sub-skills") && (
+          <Grid item xs={12}>
+            <Typography
+              variant={isSmallScreen ? "body2" : "h6"}
+              gutterBottom
+              sx={{ textAlign: "center", mb: 2 }}
+            >
+              Sub-Skills
+            </Typography>
+            <Stack
+              direction={isSmallScreen ? "column" : "row"}
+              spacing={1}
+              gap={1}
+              alignItems="center"
+              justifyContent="center"
+              flexWrap="wrap"
+            >
+              {skillExperience["Sub-skills"].map((skill, index) => (
+                <StyledChip
+                  label={skill}
+                  key={index}
+                  sx={{
+                    width: isSmallScreen
+                      ? "100%"
+                      : isMediumScreen
+                      ? "50%"
+                      : "auto",
+                  }}
+                />
+              ))}
+            </Stack>
+          </Grid>
+        )}
+      </Grid>
     </EmorphedBox>
   );
 };
---
+++ src/components/Experience/experienceData.js
@@ -0,0 +1,13 @@
+export const experienceData = {
+  Spring: {
+    Overview:
+      "I have used Spring Boot in various projects for creating RESTful services, building robust and scalable backend applications.",
+    "Sub-skills": [
+      "Spring Security",
+      "Spring Data JPA",
+      "Spring Exception Handler",
+      "Spring Boot Actuator",
+      "Spring Boot DevTools",
+    ],
+  },
+};
