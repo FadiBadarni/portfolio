9abcdbf96496ec5032ab8fbb68d7e53c34028ea4
Fadi <fadybd1@gmail.com>
2023-09-27 16:56:38 +0300

    dummy data for experience

--- src/components/Experience/experienceData.js
+++ src/components/Experience/experienceData.js
@@ -10,4 +10,74 @@ export const experienceData = {
       "Spring Boot DevTools",
     ],
   },
+  HTML: {
+    Overview:
+      "Experienced in creating markup for web applications, adhering to semantic and accessible principles.",
+    "Sub-skills": ["HTML5", "Semantic HTML", "Accessibility"],
+  },
+  CSS: {
+    Overview:
+      "Skilled in styling web pages with a focus on responsive and dynamic designs.",
+    "Sub-skills": ["Flexbox", "Grid", "CSS Variables"],
+  },
+  SASS: {
+    Overview:
+      "Familiar with SASS preprocessor to create modular, maintainable styles.",
+    "Sub-skills": ["Variables", "Mixins", "Functions"],
+  },
+  Python: {
+    Overview:
+      "Utilized Python in various projects, including web development, scripting, and data analysis.",
+    "Sub-skills": ["Flask", "Django", "Pandas"],
+  },
+  Java: {
+    Overview:
+      "Experience in Java for building robust, scalable backend services and applications.",
+    "Sub-skills": ["JVM", "Collections", "Concurrency"],
+  },
+  MySQL: {
+    Overview:
+      "Proficient in MySQL for database management, creating efficient queries, and database optimization.",
+    "Sub-skills": ["Queries", "Joins", "Indexes"],
+  },
+  PostgreSQL: {
+    Overview:
+      "Used PostgreSQL in projects requiring relational database solutions, capable of handling complex queries.",
+    "Sub-skills": ["PL/pgSQL", "Window Functions", "CTEs"],
+  },
+  JavaScript: {
+    Overview:
+      "Solid understanding of JavaScript for both client-side and server-side development.",
+    "Sub-skills": ["ES6+", "Async/Await", "DOM Manipulation"],
+  },
+  React: {
+    Overview:
+      "Experience building single-page applications using React, familiar with hooks and state management.",
+    "Sub-skills": ["Hooks", "Redux", "Context API"],
+  },
+  Docker: {
+    Overview:
+      "Used Docker for containerizing applications, aiding in deployment and scalability.",
+    "Sub-skills": ["Docker Compose", "Dockerfile", "Containerization"],
+  },
+  TypeScript: {
+    Overview:
+      "Employed TypeScript for adding static typing to JavaScript, enhancing code quality and readability.",
+    "Sub-skills": ["Static Typing", "Interfaces", "Generics"],
+  },
+  C: {
+    Overview:
+      "Basic understanding of C for systems programming and resource management.",
+    "Sub-skills": ["Pointers", "Memory Management", "Structs"],
+  },
+  "C++": {
+    Overview:
+      "Intermediate level C++ for system-level programming, data structures, and algorithms.",
+    "Sub-skills": ["OOP", "STL", "Templates"],
+  },
+  Next: {
+    Overview:
+      "Beginner-level experience with Next.js for server-rendered React applications.",
+    "Sub-skills": ["SSR", "ISR", "API Routes"],
+  },
 };
